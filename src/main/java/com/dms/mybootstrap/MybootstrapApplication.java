83586aba0dec2f8f92eb35af13b8831bc33e6135
BrightSt <dongmingsheng20@163.com>
2018-11-11 18:18:55 +0800

    222

---
+++ src/main/java/com/dms/mybootstrap/MybootstrapApplication.java
@@ -0,0 +1,12 @@
+package com.dms.mybootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybootstrapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybootstrapApplication.class, args);
+    }
+}
---
+++ src/main/java/com/dms/mybootstrap/controller/IndexController.java
@@ -0,0 +1,4 @@
+package com.dms.mybootstrap.controller;
+
+public class IndexController {
+}
